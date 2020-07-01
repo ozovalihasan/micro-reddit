5f4dde7a1ececb1af9eb668348a6f384f1a9be5d
imhilla <hillaryodhiambo282@gmail.com>
2020-07-01 17:47:36 +0300

    Add validators for comments and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user 
   has_many :comments
+  validates :title, presence: true, length: { in: 6..30 }
+  validates :body, presence: true, length: { minimum: 10}
 end
